5053e8f76704a881b770fe1732fe31930cc4d2c6
Saulo Coelho <coelho.saulo@gmail.com>
2025-02-22 16:57:36 -0300

    update 101-lazy_matrix_mul.txt

--- python-test_driven_development/101-lazy_matrix_mul.py
+++ python-test_driven_development/101-lazy_matrix_mul.py
@@ -15,20 +15,6 @@ def lazy_matrix_mul(m_a, m_b):
     if not isinstance(m_a, list) or not isinstance(m_b, list):
         raise ValueError("Scalar operands are not allowed, use '*' instead")
         
-    # Verifica se as matrizes são listas de listas e se contêm apenas números
-    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
-        raise TypeError("invalid data type for einsum")
-        
-    # Verifica se todas as linhas têm o mesmo número de colunas
-    if any(len(row) != len(m_a[0]) for row in m_a):
-        raise ValueError("setting an array element with a sequence.")
-
-    if any(len(row) != len(m_b[0]) for row in m_b):
-        raise ValueError("setting an array element with a sequence.")
-
-    if not all(isinstance(num, (int, float)) for row in m_a for num in row) or not all(isinstance(num, (int, float)) for row in m_b for num in row):
-        raise TypeError("invalid data type for einsum")
-        
     # Obtém as dimensões das matrizes
     rows_a = len(m_a)
     cols_a = len(m_a[0]) if rows_a > 0 else 0
@@ -42,6 +28,20 @@ def lazy_matrix_mul(m_a, m_b):
     # Verifica se as dimensões permitem a multiplicação
     if cols_a != rows_b:
         raise ValueError(f"shapes ({rows_a},{cols_a}) and ({rows_b},{cols_b}) not aligned: {cols_a} (dim 1) != {rows_b} (dim 0)")
+        
+    # Verifica se as matrizes são listas de listas e se contêm apenas números
+    if not all(isinstance(row, list) for row in m_a) or not all(isinstance(row, list) for row in m_b):
+        raise TypeError("invalid data type for einsum")
+        
+    # Verifica se todas as linhas têm o mesmo número de colunas
+    if any(len(row) != len(m_a[0]) for row in m_a):
+        raise ValueError("setting an array element with a sequence.")
+
+    if any(len(row) != len(m_b[0]) for row in m_b):
+        raise ValueError("setting an array element with a sequence.")
+
+    if not all(isinstance(num, (int, float)) for row in m_a for num in row) or not all(isinstance(num, (int, float)) for row in m_b for num in row):
+        raise TypeError("invalid data type for einsum")
         
 
     return (np.matmul(m_a, m_b))
